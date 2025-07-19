aa229e2a58ddac3fd6450d415634409161796609
mari-yudina <mari.yudina1983@ukr.net>
2025-07-19 19:30:00 +0300

    update

---
+++ src/main.js
@@ -0,0 +1,62 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { getImagesByQuery } from './js/pixabay-api.js';
+import { createGallery } from './js/render-functions.js';
+import { clearGallery } from './js/render-functions.js';
+import { showLoader } from './js/render-functions.js';
+import { hideLoader } from './js/render-functions.js';
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const query = input.value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      title: 'Порожнє поле',
+      message: 'Введи ключове слово для пошуку зображень!',
+      position: 'topCenter',
+      timeout: 3000,
+    });
+    
+
+    input.focus();
+    return;
+  }
+  
+showLoader();
+ getImagesByQuery(query)
+ .then(data => {
+  hideLoader();
+  if(data.hits.length === 0){
+    form.reset();
+    clearGallery();
+    iziToast.warning({
+      message: 'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topCenter',
+      timeout: 3000,
+    });
+   
+
+    return; 
+  }
+  clearGallery();
+  createGallery(data.hits);
+  form.reset();
+ 
+})
+.catch(error => {
+  hideLoader();
+  
+      iziToast.error({
+        title: 'Помилка',
+        message: `Не вдалося отримати зображення: ${error.message}`,
+        position: 'topRight',
+        timeout: 3000,
+      });
+    });
+});
